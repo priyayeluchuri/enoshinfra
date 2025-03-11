637dfdbf8fcfc65ebefe1c30bd846a141b1168b2
Priya Thonduri <priya.thonduri@canzuki.com>
2025-03-11 12:11:11 +0530

    sitemap pushed to google property

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.enoshinfra.com',
+  siteUrl: 'https://enoshinfra.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'weekly',
